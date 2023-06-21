7b05991fbdfc1c0d115b7c2ae6df1f1bc34e6233
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2023-06-21 08:55:21 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -ex
 echo "Install and Running Appium Server V2 as a Background process"
 
 sudo npm install -g appium@next
-appium driver install xcuitest
-appium driver install espresso
-appium driver install flutter
+appium driver install uiautomator2
 
 appium -v
 appium --log appium.log &>/dev/null &
